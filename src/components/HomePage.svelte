2bc612625e31b86d96c869322ea99e34866ef5fa
ellsphillips <elliott.phillips.dev@gmail.com>
2023-05-06 19:02:45 +0100

    Dev: tailwind config

--- src/components/HomePage.svelte
+++ src/components/HomePage.svelte
@@ -2,8 +2,8 @@
     import "../app.css";
 </script>
 
-<div class="w-96 h-screen flex flex-col justify-center items-center m-auto">
-    <h1 class="text-4xl">
+<div class="bg-grey-100 w-96 h-screen flex flex-col justify-center items-center m-auto">
+    <h1 class="text-4xl text-red-500">
         <pre>gov-learn</pre>
     </h1>
     <a href="https://www.github.com/ellsphillips/gov-learn">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,33 @@
 export default {
-  plugins: [],
-    theme: {
-    extend: {},
-  },
-  purge: ["./index.html",'./src/**/*.{svelte,js,ts}'],
-  variants: {
-    extend: {},
-  },
-  darkMode: "media"
-}
+	content: ["./src/**/*.{html,js,svelte,ts}"],
+	theme: {
+		extend: {
+			screens: {
+				xs: "475px",
+				xxs: "200px",
+			},
+			borderRadius: {
+				DEFAULT: "3px",
+			},
+			colors: {
+				"gov-sun-yellow": "#fbc900",
+				// Greyscale
+				"gov-black": "#222222",
+				"gov-grey-100": "#414042",
+				"gov-grey-75": "#707071",
+				"gov-grey-55": "#969697",
+				"gov-grey-35": "#bcbcbd",
+				"gov-grey-15": "#e2e2e3",
+				"gov-grey-5": "#f5f5f6",
+				"gov-off-white": "#fbfbfb",
+				"gov-white": "#ffffff",
+			},
+			fontFamily: {
+				sans: ["Open Sans", "Helvetica", "Arial", "sans-serif"],
+			},
+		},
+	},
+	plugins: [],
+};
+
+
