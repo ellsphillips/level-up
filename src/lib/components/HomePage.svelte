2bbe9f319a935784f9809086e8d5dfff6d4ceed5
ellsphillips <elliott.phillips.dev@gmail.com>
2023-05-07 17:56:06 +0100

    Dev: interim home page

---
+++ src/lib/components/HomePage.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import "../../app.css";
+</script>
+
+<div class="border-level-up-orange border-4 rounded-lg p-8">
+    <div class="w-96 h-full flex flex-col justify-center items-center m-auto">
+        <h1 class="text-4xl">
+            <pre>level-up</pre>
+        </h1>
+        <a href="https://www.github.com/ellsphillips/level-up">
+            <p>Accelerate your learning &amp; development</p>
+        </a>
+    </div>
+</div>
