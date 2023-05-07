36cddb90671d768dcb7d30e3513f4bdd18b91457
ellsphillips <elliott.phillips.dev@gmail.com>
2023-05-07 17:53:37 +0100

    Enhance: article card container

---
+++ src/lib/components/ArticleCard.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Tag from "$components/Tag.svelte";
+
+    export let data: Article;
+</script>
+
+<article class="space-y-2">
+    <span class="font-lg text-level-up-grey-35">{data.date}</span>
+    <div class="space-y-2">
+        <div class="space-y-1">
+            <h2 class="text-2xl font-bold">{data.title}</h2>
+            <div class="flex">
+                {#each data.tags as tag}
+                    <Tag>{tag}</Tag>
+                {/each}
+            </div>
+        </div>
+        <p class="text-level-up-grey-55">{data.description}</p>
+    </div>
+</article>
