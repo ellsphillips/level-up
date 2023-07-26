986eefbcd0b4409ad6a42e84810c9e0a37414ce0
ellsphillips <elliott.phillips.dev@gmail.com>
2023-07-26 15:02:49 +0100

    Feature: hero

---
+++ components/Hero.tsx
@@ -0,0 +1,17 @@
+export default function Hero() {
+  return (
+    <div className="mt-64 mb-12 md:mt-28 lg:my-28">
+      <h1 className="pb-12 text-5xl font-bold">
+        Level-up your
+        <br />
+        developer toolkit
+        <br />
+        and mindset
+      </h1>
+      <p className="text-lg">
+        Accelerate your learning in software development and bridge <br />
+        the gap between junior and senior roles
+      </p>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,9 @@
+import Hero from "@/components/Hero";
 import Layout from "@/layouts/App";
-import Link from "next/link";
 
 const IndexPage = () => (
   <Layout title="LEVEL-UP! - Home">
-    <h1 className="font-black">LEVEL-UP!</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
+    <Hero />
   </Layout>
 );
 
