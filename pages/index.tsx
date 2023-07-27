71ef75d1c305c750019b3c78ece9c1103332eee5
ellsphillips <elliott.phillips.dev@gmail.com>
2023-07-27 08:41:39 +0100

    Dev: merge histories

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Hero from "@/components/Hero";
 import Layout from "@/layouts/App";
 
@@ -9,18 +8,3 @@ const IndexPage = () => (
 );
 
 export default IndexPage;
-=======
-import Link from 'next/link'
-import Layout from '../components/Layout'
-
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-)
-
-export default IndexPage
->>>>>>> 09feaf9fa7a2d26a60b082e43e7e9bbddd922b1d
