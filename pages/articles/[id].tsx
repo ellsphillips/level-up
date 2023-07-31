9b8daf53887e2978dabb48df8ac2c51054d776be
ellsphillips <elliott.phillips.dev@gmail.com>
2023-07-31 19:51:01 +0100

    Dev: tidy up

--- pages/articles/[id].tsx
+++ pages/articles/[id].tsx
@@ -1,11 +1,11 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 
 import ListDetail from '@/components/article/Details';
+import SeriesTOC from '@/components/article/SeriesTOC';
 import { articleData } from '@/data/articles';
 import lipsum from '@/data/lipsum';
 import Layout from '@/layouts/Article';
 import { Article, Series } from '@/types/article';
-import Link from 'next/link';
 import { useEffect, useState } from 'react';
 
 type Props = {
@@ -36,47 +36,15 @@ const StaticPropsDetail = ({ item, errors }: Props) => {
 
     return (
         <Layout title={item ? item.title : 'Article Detail'}>
-            <div className='py-16'>{item && <ListDetail item={item} />}</div>
+            <div className='py-12'>{item && <ListDetail item={item} />}</div>
 
             <div className='flex min-h-full min-w-0 flex-col items-center justify-center shadow-xl mt-4 overflow-hidden rounded-lg bg-gray-800 ring-1 ring-inset ring-white/5 aspect-[2/1]'></div>
 
-            <div className='flex gap-8 pt-16'>
-                <aside className='w-1/3'>
-                    <a className='text-xl font-medium text-white sm:font-semibold'>
-                        {item?.series}
-                    </a>
-                    <p className='mt-1 text-sm text-gray-500 sm:text-base'>
-                        {articlesInSeries.length} lessons · 24 minute read
-                    </p>
-                    <div className='mt-4 space-y-1'>
-                        {articlesInSeries.map((article, i) => {
-                            return (
-                                <Link
-                                    href='/articles/[id]'
-                                    as={`/articles/${article.id}`}
-                                    className='flex items-center justify-between'
-                                >
-                                    <div
-                                        className={`flex items-center w-full px-4 py-2 text-sm rounded hover:bg-slate-900 hover:cursor-pointer ${
-                                            item.id === article.id &&
-                                            'bg-slate-800'
-                                        }`}
-                                    >
-                                        <span className='text-gray-500'>
-                                            {i + 1}
-                                        </span>
-                                        <p className='flex-1 ml-4 text-white truncate'>
-                                            {article.title}
-                                        </p>
-                                        <span className='ml-2 text-gray-500'>
-                                            8 mins
-                                        </span>
-                                    </div>
-                                </Link>
-                            );
-                        })}
-                    </div>
-                </aside>
+            <div className='flex flex-col gap-8 pt-16 md:flex-row'>
+                <SeriesTOC
+                    series={articlesInSeries}
+                    className='w-full md:w-1/3'
+                />
                 <article className='flex-1'>{lipsum}</article>
             </div>
         </Layout>
