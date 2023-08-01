1d0f05fc0579bd812ea8726b5dc259beb25fc514
ellsphillips <elliott.phillips.dev@gmail.com>
2023-08-01 09:23:17 +0100

    Dev: move tags helper to lib

--- pages/tags/[tag]/index.tsx
+++ pages/tags/[tag]/index.tsx
@@ -1,32 +1,32 @@
-import { Tag } from "@/types/article";
-import { getUniqueTags } from "@/utils/tags";
+import { getUniqueTags } from '@/lib/tags';
+import { Tag } from '@/types/article';
 
 export default function TagPage() {
-  const tags = getUniqueTags();
-  const tag: Tag = {
-    name: "tag",
-    count: 4,
-  };
+    const tags = getUniqueTags();
+    const tag: Tag = {
+        name: 'tag',
+        count: 4,
+    };
 
-  return (
-    // <div className="divide-y-2 divide-level-up-grey-75">
-    //   <h1 className="pb-8 text-6xl font-bold">#{tag}</h1>
+    return (
+        // <div className="divide-y-2 divide-level-up-grey-75">
+        //   <h1 className="pb-8 text-6xl font-bold">#{tag}</h1>
 
-    //   <div>
-    //     {tags.includes(tag.name) ? (
-    //       <p className="py-4 text-xl">No tags found that match "{tag}".</p>
-    //     ) : (
-    //       articleData.map(
-    //         (article) =>
-    //           article.tags.includes(tag.name) && (
-    //             <div className="py-4">
-    //               <Article {...article} />
-    //             </div>
-    //           )
-    //       )
-    //     )}
-    //   </div>
-    // </div>
-    <p>tags</p>
-  );
+        //   <div>
+        //     {tags.includes(tag.name) ? (
+        //       <p className="py-4 text-xl">No tags found that match "{tag}".</p>
+        //     ) : (
+        //       articleData.map(
+        //         (article) =>
+        //           article.tags.includes(tag.name) && (
+        //             <div className="py-4">
+        //               <Article {...article} />
+        //             </div>
+        //           )
+        //       )
+        //     )}
+        //   </div>
+        // </div>
+        <p>tags</p>
+    );
 }
