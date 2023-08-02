109aaadd263f9bcbe62a573f6624db3659fc0a4e
ellsphillips <elliott.phillips.dev@gmail.com>
2023-08-02 14:34:21 +0100

    Dev: setup global, tailwind, and prism styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
-import { AppProps } from "next/app";
+import { AppProps } from 'next/app';
 
-import "@/styles/global.css";
+import '@/styles/global.css';
+import '@/styles/prism.css';
+import '@/styles/tailwind.css';
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    return <Component {...pageProps} />;
 }
 
 export default App;
