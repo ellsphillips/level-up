76e9099d11413f4aa095e129a651179799f94b18
ellsphillips <elliott.phillips.dev@gmail.com>
2023-08-06 18:58:07 +0100

    Feature: article draft placeholder

---
+++ components/Draft.tsx
@@ -0,0 +1,12 @@
+export default function Draft(): React.ReactElement {
+    return (
+        <div className='mt-24 text-center'>
+            <h1>
+                Under Construction{' '}
+                <span role='img' aria-label='roadwork sign'>
+                    🚧
+                </span>
+            </h1>
+        </div>
+    );
+}
