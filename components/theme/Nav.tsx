df3599c651f970e55ff85c4872e9ffd07c1524d4
ellsphillips <elliott.phillips.dev@gmail.com>
2023-08-10 14:41:31 +0100

    Feature: pass theme hook to top-level app props

--- components/theme/Nav.tsx
+++ components/theme/Nav.tsx
@@ -1,16 +1,29 @@
 import Link from 'next/link';
 import Logo from '../Logo';
 
+import ThemeToggle from '@/components/theme/themeToggle';
+
+import { useTheme } from 'next-themes';
+
 const Nav = () => {
+    const { theme, setTheme } = useTheme();
+
     return (
         <header className='flex-col items-center justify-between pb-8 space-y-4 sm:flex sm:flex-row sm:space-y-0'>
             <Link as='/' href='/'>
                 <Logo />
             </Link>
-            <nav className='flex space-x-8 sm:justify-end'>
-                <Link href='/articles'>Articles</Link>
-                <Link href='/pathways'>Pathways</Link>
-            </nav>
+            <div className='flex items-center space-x-8'>
+                <nav className='flex space-x-8 sm:justify-end'>
+                    <Link href='/articles'>Articles</Link>
+                    <Link href='/pathways'>Pathways</Link>
+                </nav>
+                <ThemeToggle
+                    onClick={() =>
+                        setTheme(theme === 'dark' ? 'light' : 'dark')
+                    }
+                />
+            </div>
         </header>
     );
 };
