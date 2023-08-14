f901c87c3cdcee36865698b44493407984cd43b5
ellsphillips <elliott.phillips.dev@gmail.com>
2023-08-14 20:01:06 +0100

    Feature: render all content cards for each pathway at index

--- pages/pathways/index.tsx
+++ pages/pathways/index.tsx
@@ -2,7 +2,36 @@ import Footer from '@/components/theme/Footer';
 import Nav from '@/components/theme/Nav';
 import Head from 'next/head';
 
-export default function Pathways() {
+import Card from '@/components/pathway/Card';
+import { getAllFilesFrontMatter } from '@/lib/mdx';
+import { FrontMatter } from '@/types/article';
+import config from 'config';
+import { GetStaticProps, InferGetStaticPropsType } from 'next';
+
+// @ts-ignore
+export const getStaticProps: GetStaticProps<{
+    allFrontMatter: FrontMatter[];
+}> = async () => {
+    const allFrontMatter = await getAllFilesFrontMatter('pathways');
+
+    return {
+        props: {
+            allFrontMatter,
+        },
+    };
+};
+
+export default function Pathway({
+    allFrontMatter,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+    const { pathways } = config;
+
+    const pathwayInfo = pathways.map(pathway => ({
+        title: pathway.title,
+        slug: pathway.slug,
+        articles: allFrontMatter.filter(fm => fm.slug.includes(pathway.slug)),
+    }));
+
     return (
         <>
             <Head>
@@ -13,10 +42,33 @@ export default function Pathways() {
                     content='initial-scale=1.0, width=device-width'
                 />
             </Head>
+
             <div className='max-w-6xl p-8 mx-auto'>
                 <Nav />
 
-                <h1 className='text-4xl font-bold'>Pathways</h1>
+                <h1 className='py-8 text-4xl font-bold'>Pathways</h1>
+
+                <div className='divide-y-2 divide-lu-grey-700/25'>
+                    {pathwayInfo.map(pathway => (
+                        <section className='py-16 mx-auto space-y-8 max-w-7xl'>
+                            <h2 className='text-2xl font-bold text-lu-grey-800 dark:text-lu-grey-100'>
+                                {pathway.title}
+                            </h2>
+                            <div className='grid gap-8 lg:grid-cols-3'>
+                                {pathway.articles.map((article, i) => (
+                                    <Card
+                                        id={i + 1}
+                                        title={article.title}
+                                        readingTime={article.readingTime}
+                                        thumbnail={`pathways/${article.slug}`}
+                                        description={article.summary}
+                                        slug={article.slug}
+                                    />
+                                ))}
+                            </div>
+                        </section>
+                    ))}
+                </div>
 
                 <Footer />
             </div>
