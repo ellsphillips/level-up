7fe24521a957f29f24bcab1637d71038e4008cd8
ellsphillips <elliott.phillips.dev@gmail.com>
2023-08-19 18:43:24 +0100

    Feature: ddat cta section

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,11 @@ import Card from '@/components/pathway/Card';
 import Section from '@/components/theme/Section';
 import { articleData } from '@/data/articles';
 
+import Link from '@/components/Link';
+import PathwayIcon, { type PathwayID } from '@/components/pathway/Icon';
 import Footer from '@/components/theme/Footer';
 import Nav from '@/components/theme/Nav';
+import slugify from '@/lib/utils/slugify';
 
 const Home = () => (
     <>
@@ -28,12 +31,92 @@ const Home = () => (
             </Section>
 
             <Section className='py-8 space-y-8 lg:-top-16'>
-                <h2 className='text-2xl font-bold'>Career pathways</h2>
+                <h2 className='text-2xl font-bold'>New pathway launched!</h2>
 
                 <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3'>
-                    {articleData.map((article, i) => (
-                        <Card key={i} {...article} />
+                    {articleData.slice(0, 3).map((article, i) => (
+                        <Card
+                            key={i}
+                            {...article}
+                            slug={`${slugify(article.pathway)}/${slugify(
+                                article.title,
+                            )}`}
+                        />
                     ))}
+                </div>
+            </Section>
+
+            <Section cta className='py-12 space-y-4'>
+                <h2 className='text-4xl font-bold'>The DDaT framework</h2>
+                <div className='mt-4 grid gap-x-3.5 lg:mt-11 lg:grid-cols-3'>
+                    <div className='lg:col-span-2 lg:pr-20'>
+                        <p className='text-lg font-medium lg:text-2xl'>
+                            LEVEL-UP! uses Government's DDaT framework to shape
+                            the future of technical learning.
+                        </p>
+                        <p className='mt-8 space-y-4 text-gray-300 md:grid md:grid-cols-2 md:gap-6 md:space-y-0 lg:mt-6 lg:block lg:space-y-4 lg:text-lg'>
+                            As a digital, data and technology specialist you'll
+                            work at the cutting edge of how people interact, and
+                            are empowered by digital government services.
+                            Government is undergoing a digital transformation,
+                            never has the time been better to pursue your career
+                            in a sector, in a role, that truly delivers on the
+                            promise of innovation.
+                        </p>
+
+                        <Link
+                            href='https://www.gov.uk/government/collections/digital-data-and-technology-profession-capability-framework'
+                            className='inline-block mt-8 font-semibold hyperlink text-accent hover:text-accent-light'
+                        >
+                            Find out more →
+                        </Link>
+                    </div>
+                    <div className='flex flex-col items-center w-full max-w-md px-2 mx-auto rounded-lg mt-14 lg:-mt-9 lg:items-start'>
+                        <div className='flex items-center w-full space-x-4'>
+                            {[
+                                'data-engineer-toolkit',
+                                'data-scientist-lab',
+                                'software-developer-mindset',
+                            ].map((level, i) => (
+                                <div key={i} className='h-8'>
+                                    <PathwayIcon pathway={level as PathwayID} />
+                                </div>
+                            ))}
+                        </div>
+                        <p className='mt-2 text-lg font-medium text-white lg:text-base'>
+                            Subscribe to be notified when new content is
+                            available
+                        </p>
+                        <div className='w-full mt-10'>
+                            <form>
+                                <label
+                                    htmlFor='email'
+                                    className='text-sm font-medium text-white'
+                                >
+                                    Enter your email
+                                </label>
+                                <div className='flex flex-col mt-2 rounded-md shadow-sm lg:flex-row'>
+                                    <div className='relative flex items-stretch flex-grow focus-within:z-10'>
+                                        <input
+                                            id='email'
+                                            name='email'
+                                            type='email'
+                                            required={true}
+                                            data-testid='checkout-email'
+                                            className='block w-full border-gray-400 rounded-md bg-white/5 focus-visible:border-white focus-visible:outline-none focus-visible:ring-0 sm:text-sm lg:rounded-none lg:rounded-l-md'
+                                        />
+                                    </div>
+                                    <button
+                                        className='relative px-4 py-2 mt-4 -ml-px space-x-2 overflow-hidden font-semibold text-white border rounded-md border-accent bg-accent hover:bg-accent-light focus-visible:border-white focus-visible:outline-none lg:mt-0 lg:rounded-l-none lg:rounded-r-md lg:border-gray-400 lg:py-2 lg:text-sm lg:focus-visible:border-white '
+                                        type='submit'
+                                        data-testid='checkout-button'
+                                    >
+                                        <span>Sign up →</span>
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </Section>
         </main>
