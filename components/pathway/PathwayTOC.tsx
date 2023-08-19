7a2fa70c1e15ceb876dc775c15ba3d49a53e2717
ellsphillips <elliott.phillips.dev@gmail.com>
2023-08-19 16:22:32 +0100

    Feature: theme adaptive pathway toc

--- components/pathway/PathwayTOC.tsx
+++ components/pathway/PathwayTOC.tsx
@@ -1,17 +1,20 @@
-import slugify from '@/lib/utils/slugify';
-import { Article } from '@/types/article';
+import { PathwayContent } from 'config/pathways';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
-interface SeriesTOCProps extends React.HTMLAttributes<HTMLElement> {
-    series: Article[];
+interface PathwayTOCProps extends React.HTMLAttributes<HTMLElement> {
+    slug: string;
+    series: PathwayContent[];
     children?: React.ReactNode;
 }
 
-export default function SeriesTOC(props: SeriesTOCProps) {
+export default function PathwayTOC(props: PathwayTOCProps) {
+    const router = useRouter();
+
     return (
         <aside>
             <a className='text-xl font-medium text-white sm:font-semibold'>
-                {props.series[0]?.series}
+                {props.series[0]?.name}
             </a>
             <p className='mt-1 text-sm text-gray-500 sm:text-base'>
                 {props.series.length} lessons · 24 minute read
@@ -20,20 +23,20 @@ export default function SeriesTOC(props: SeriesTOCProps) {
                 {props.series.map((article, i) => {
                     return (
                         <Link
-                            href='/articles/[slug]'
-                            as={`/articles/${slugify(article.title)}`}
+                            href='/pathways/[...slug]'
+                            as={`/pathways/${props.slug}/${article.slug}`}
                             className='flex items-center justify-between'
                         >
                             <div
-                                className={`flex items-center w-full px-4 py-2 text-sm rounded hover:bg-slate-800 hover:cursor-pointer ${
-                                    location.pathname ===
-                                        `/articles/${slugify(article.title)}` &&
-                                    'bg-slate-900'
+                                className={`flex items-center w-full px-4 py-2 text-sm rounded hover:bg-neutral-200 dark:hover:bg-slate-700 hover:cursor-pointer ${
+                                    router.asPath.split('/').pop() ===
+                                        article.slug &&
+                                    'bg-neutral-100 dark:bg-slate-800'
                                 }`}
                             >
                                 <span className='text-gray-500'>{i + 1}</span>
-                                <p className='flex-1 ml-4 text-white truncate'>
-                                    {article.title}
+                                <p className='flex-1 ml-4  max-w-[12rem] truncate'>
+                                    {article.name}
                                 </p>
                                 <span className='ml-2 text-gray-500'>
                                     8 mins
